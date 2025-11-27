8f1f125ffb60996cc187ae93a08600648b5727e8
Sergio Casal <sergiiiocasal@gmail.com>
2025-11-28 00:58:00 +0100

    cambios

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import app from "./src/app.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: "https://naturalpeluqueria1.netlify.app/", 
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
